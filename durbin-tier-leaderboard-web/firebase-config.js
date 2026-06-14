a94c075c5130fe53bf5baa88cba0ed9fcd5d4c4e
iamcocotheking-cmd <iamcocotheking@gmail.com>
2026-06-14 10:39:04 +0600

    Delete durbin-tier-leaderboard-web directory

--- durbin-tier-leaderboard-web/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyA-3a8G5mGpRShkPstscJwt26Nx8mhlGUU",
-  authDomain: "durbin-news-7cff2.firebaseapp.com",
-  projectId: "durbin-news-7cff2",
-  storageBucket: "durbin-news-7cff2.firebasestorage.app",
-  messagingSenderId: "207243018045",
-  appId: "1:207243018045:web:3a83180c63498647848f54",
-  measurementId: "G-DW7ZWNRC3F"
-};
